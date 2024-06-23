477d15947d6b716f7094576e6a26b42cd83d92f1
haroyan888 <haroyan3020@gmail.com>
2024-06-24 01:19:31 +0900

    id検索, 全検索作成完了

--- backend/src/handlers.rs
+++ backend/src/handlers.rs
@@ -16,3 +16,10 @@ pub async fn find_todo<T: TodoRepository>(
     let todo = repository.find(id).await.or(Err(StatusCode::NOT_FOUND))?;
     Ok((StatusCode::OK, Json(todo)))
 }
+
+pub async fn all_todo<T: TodoRepository> (
+    Extension(repository): Extension<Arc<T>>
+) -> Result<impl IntoResponse, StatusCode> {
+    let todos = repository.all().await.or(Err(StatusCode::))
+    Ok((StatusCode::OK, Json(todos)))
+}
